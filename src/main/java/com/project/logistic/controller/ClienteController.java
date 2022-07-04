e03c78303831271e296091a4058d59b41a2c6914
igorsalgado <salgado.igor.is@gmail.com>
2022-07-04 14:51:33 -0300

    Implementando CRUD Cliente

--- src/main/java/com/project/logistic/controller/ClienteController.java
+++ src/main/java/com/project/logistic/controller/ClienteController.java
@@ -4,6 +4,7 @@ import com.project.logistic.domain.model.Cliente;
 import com.project.logistic.domain.repository.ClienteRepository;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,9 +29,32 @@ public class ClienteController {
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build()); // retorna o cliente se existir, senão retorna 404
     }
-
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED) // indica que o método retorna um status 201
     public Cliente adicionar(@RequestBody Cliente cliente) {
-        return clienteRepository.save(cliente);
+        return clienteRepository.save(cliente); // adiciona e salva cliente e retorna 201
     }
+
+    @PutMapping("/{clienteId}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, @RequestBody Cliente cliente) {
+        if (!clienteRepository.existsById(clienteId)) {
+            return ResponseEntity.notFound().build(); // retorna 404 se o cliente não existir
+        }
+
+        cliente.setId(clienteId); // atualiza o id do cliente
+        cliente = clienteRepository.save(cliente); // salva o cliente
+        return ResponseEntity.ok(cliente); // retorna o cliente atualizado
+
+    }
+
+    @DeleteMapping("/{clienteId}")
+    public ResponseEntity<Void> remover(@PathVariable Long clienteId) {
+        if (!clienteRepository.existsById(clienteId)) {
+            return ResponseEntity.notFound().build(); // retorna 404 se o cliente não existir
+        }
+
+        clienteRepository.deleteById(clienteId); // remove o cliente
+        return ResponseEntity.noContent().build(); // retorna 204
+    }
+
 }
